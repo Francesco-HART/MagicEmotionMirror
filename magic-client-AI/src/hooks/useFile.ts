6d9bd5fb9f93a4f868859c1e6f6042443004479c
Francesco <hart.francesco@gmail.com>
2022-11-03 18:04:25 +0100

    add

--- magic-client-AI/src/hooks/useFile.ts
+++ magic-client-AI/src/hooks/useFile.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://10.49.33.120:5000/api",
+  baseURL: "http://192.168.61.54:5000/api",
 });
 const useFile = () => {
   const convertCanavasToFileSystemAndSend = async (
